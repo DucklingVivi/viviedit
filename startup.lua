b6ba27364db80e1a5f357c1c949823296ee4faee
DucklingVivi <95558019+DucklingVivi@users.noreply.github.com>
2024-11-27 17:40:25 -0500

    bird

--- startup.lua
+++ startup.lua
@@ -3,8 +3,10 @@ local completion = require "cc.shell.completion"
 
 
 shell.setCompletionFunction("viviedit/viviedit.lua", completion.build(completion.file))
+shell.setCompletionFunction("viviedit/cosu.lua", completion.build(completion.file))
 
-shell.setAlias("spelledit", "viviedit/viviedit.lua")
+
+shell.setAlias("spelledit", "viviedit/cosu.lua")
 shell.setAlias("vupdate", "viviedit/vividownload.lua")
 
 settings.load(".settings")
